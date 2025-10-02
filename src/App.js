8b3fa624d991b1dfb577850048a9436788d6e2e4
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-03 02:22:01 +0900

    📦 chore: install map-related packages
    - add leaflet for map rendering functionality
    - add react-leaflet for ease to use the leaflet
    - add leaflet-defaulticon-compatibility for default icon support
    - add static elements in the App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 export default function App() {
-  return <div>TEST</div>;
+  const position = [37.576508, 126.983451]; // 서울공예박물관 (지도 중앙)
+  const spot = [37.576832, 126.976724]; // 경복궁 흥례문 앞 <궁중문화축전> 종합안내소
+  // const spot = [37.578792, 126.996511]; // 창경궁 홍화문 앞 <궁중문화축전> 종합안내소
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <MapContainer
+        center={position}
+        zoom={17}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+        <Marker position={spot}>
+          <Popup>경복궁 궁중문화축전 종합안내소</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
