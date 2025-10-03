486f21c2c7c2fb145e55166c3a8c2ea67b4155da
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-04 00:54:09 +0900

    ✨ feat: add the marker of each spot on the map
    - add the data of each spot as an array named spots
    - display the marker of each spot by using array.map()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export default function App() {
-  const position = [37.576508, 126.983451]; // 서울공예박물관 (지도 중앙)
-  const spot = [37.576832, 126.976724]; // 경복궁 흥례문 앞 <궁중문화축전> 종합안내소
-  // const spot = [37.578792, 126.996511]; // 창경궁 홍화문 앞 <궁중문화축전> 종합안내소
+  const position = [37.574419, 126.982628]; // 우정총국 (지도 중앙)
+  const spots = [
+    {
+      id: 10,
+      name: '경복궁 종합안내소 (흥례문)',
+      lat: 37.576832,
+      lng: 126.976724,
+      hasStamp: false,
+    },
+    {
+      id: 11,
+      name: '경복궁 집옥재',
+      lat: 37.58346,
+      lng: 126.976075,
+      hasStamp: true,
+    },
+    {
+      id: 12,
+      name: '경복궁 향원정',
+      lat: 37.582711,
+      lng: 126.977202,
+      hasStamp: true,
+    },
+    {
+      id: 20,
+      name: '창덕궁 종합안내소 (돈화문)',
+      lat: 37.577723,
+      lng: 126.989805,
+      hasStamp: false,
+    },
+    {
+      id: 21,
+      name: '창덕궁 낙선재',
+      lat: 37.578745,
+      lng: 126.993507,
+      hasStamp: true,
+    },
+    {
+      id: 30,
+      name: '덕수궁 종합안내소 (대한문)',
+      lat: 37.565052,
+      lng: 126.976668,
+      hasStamp: false,
+    },
+    {
+      id: 31,
+      name: '덕수궁 정관헌',
+      lat: 37.566453,
+      lng: 126.975649,
+      hasStamp: true,
+    },
+    {
+      id: 32,
+      name: '덕수궁 준명당',
+      lat: 37.566326,
+      lng: 126.974737,
+      hasStamp: true,
+    },
+    {
+      id: 40,
+      name: '창경궁 종합안내소 (홍화문)',
+      lat: 37.578792,
+      lng: 126.996511,
+      hasStamp: false,
+    },
+    {
+      id: 41,
+      name: '창경궁 명정전',
+      lat: 37.578758,
+      lng: 126.99491,
+      hasStamp: true,
+    },
+    {
+      id: 42,
+      name: '창경궁 통명전',
+      lat: 37.57968,
+      lng: 126.99374,
+      hasStamp: true,
+    },
+    {
+      id: 43,
+      name: '창경궁 대온실',
+      lat: 37.582954,
+      lng: 126.994051,
+      hasStamp: true,
+    },
+    {
+      id: 51,
+      name: '종묘 정전',
+      lat: 37.57488,
+      lng: 126.993944,
+      hasStamp: true,
+    },
+    {
+      id: 52,
+      name: '종묘 영녕전',
+      lat: 37.575926,
+      lng: 126.992683,
+      hasStamp: true,
+    },
+  ];
 
   return (
     <div style={{ height: '100vh' }}>
       <MapContainer
         center={position}
-        zoom={17}
+        zoom={15}
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
-        <Marker position={spot}>
-          <Popup>경복궁 궁중문화축전 종합안내소</Popup>
-        </Marker>
+        {spots.map((spot) => (
+          <Marker position={[spot.lat, spot.lng]} key={spot.id}>
+            <Popup>{spot.name}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
