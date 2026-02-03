d1ba6164f75535dac9a926b96df3651acbdcb208
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-03 22:53:40 +0900

    📝 chore: add the logo image and correct other remains
    - add the logo image
    - remove the main tag from StampMap.js (move to App.js)
    - add comma to index.js (automatically)
    - remove some comments of index.html

--- src/components/StampMap.js
+++ src/components/StampMap.js
@@ -20,40 +20,38 @@ export default function StampMap({
   const linePositions = selectedSpots.map((spot) => [spot.lat, spot.lng]);
 
   return (
-    <main>
-      <MapContainer center={position} zoom={15} className="map">
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; OpenStreetMap contributors"
+    <MapContainer center={position} zoom={15} className="map">
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+
+      {linePositions.length > 1 && (
+        <Polyline
+          positions={linePositions}
+          color="#007bff"
+          weight={4}
+          opacity={0.8}
+          dashArray="5,10"
         />
+      )}
 
-        {linePositions.length > 1 && (
-          <Polyline
-            positions={linePositions}
-            color="#007bff"
-            weight={4}
-            opacity={0.8}
-            dashArray="5,10"
-          />
-        )}
-
-        {spots.map((spot) => (
-          <Marker
-            position={[spot.lat, spot.lng]}
-            icon={
-              spot.isSelected ? violetIcon : spot.hasStamp ? blueIcon : redIcon
-            }
-            eventHandlers={{
-              click: () => onAddSpot(spot, count, finalSpot),
-            }}
-            key={spot.id}
-          >
-            <Tooltip direction="top" offset={[0, -40]}>
-              {spot.name}
-            </Tooltip>
-          </Marker>
-        ))}
-      </MapContainer>
-    </main>
+      {spots.map((spot) => (
+        <Marker
+          position={[spot.lat, spot.lng]}
+          icon={
+            spot.isSelected ? violetIcon : spot.hasStamp ? blueIcon : redIcon
+          }
+          eventHandlers={{
+            click: () => onAddSpot(spot, count, finalSpot),
+          }}
+          key={spot.id}
+        >
+          <Tooltip direction="top" offset={[0, -40]}>
+            {spot.name}
+          </Tooltip>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
