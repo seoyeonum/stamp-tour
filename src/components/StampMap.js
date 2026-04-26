4fec6264298f96a71e1d9a2afc9e999d270864dc
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-04-26 15:45:04 +0900

    ✨ feat: display actual walking routes
    - use useEffect to watch selectedSpots and fetch project OSRM HTTP API
    - display actual walking routes between each selected spots
    - modify the polyline property (remove dotted lines)

--- src/components/StampMap.js
+++ src/components/StampMap.js
@@ -6,6 +6,7 @@ import {
   Tooltip,
 } from 'react-leaflet';
 import { violetIcon, blueIcon, redIcon } from './App';
+import { useEffect, useState } from 'react';
 
 // StampMap Area
 export default function StampMap({
@@ -15,8 +16,51 @@ export default function StampMap({
   selectedSpots,
 }) {
   // selected spot의 위치 정보 배열
-  const linePositions = selectedSpots.map((spot) => [spot.lat, spot.lng]);
+  const [routePositions, setRoutePosition] = useState([]);
   const selectedCount = selectedSpots.length;
+
+  // 실제 도보 경로 계산 effect
+  useEffect(() => {
+    async function walkingRoutes() {
+      // 선택 장소가 1군데 이하라면 routePositions 초기화([])
+      if (selectedSpots.length < 2) {
+        setRoutePosition([]);
+        return;
+      }
+
+      // 선택 장소가 2군데 이상
+      let fullPath = [];
+
+      for (let i = 0; i < selectedSpots.length - 1; i++) {
+        const start = selectedSpots[i];
+        const end = selectedSpots[i + 1];
+
+        const url =
+          `https://router.project-osrm.org/route/v1/foot/` +
+          `${start.lng},${start.lat};${end.lng},${end.lat}` +
+          `?overview=full&geometries=geojson`;
+
+        try {
+          const res = await fetch(url);
+          const data = await res.json();
+
+          if (!data.routes || data.routes.length === 0) continue;
+
+          // 새로 추가하는 경로 조각
+          const newRoute = data.routes[0].geometry.coordinates.map(
+            ([lng, lat]) => [lat, lng],
+          );
+
+          // 기존 경로들에 경로 조각 붙이기
+          fullPath = [...fullPath, ...newRoute];
+        } catch (error) {
+          console.log('도보 경로 불러오기 실패:', error);
+        }
+      }
+      setRoutePosition(fullPath);
+    }
+    walkingRoutes();
+  }, [selectedSpots]);
 
   return (
     <MapContainer center={position} zoom={14} className="map">
@@ -26,13 +70,13 @@ export default function StampMap({
       />
 
       {/* selecedSpots 요소 간 연결선 출력 */}
-      {linePositions.length > 1 && (
+      {routePositions.length > 1 && (
         <Polyline
-          positions={linePositions}
+          positions={routePositions}
           color="#007bff"
           weight={4}
           opacity={0.8}
-          dashArray="5,10"
+          // dashArray="5,10"
         />
       )}
 
